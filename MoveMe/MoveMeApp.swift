fa06a8fc0a665f6b940a2391c68d4748e94f02e6
Yaroslav Trach <zhbr282@gmail.com>
2024-06-29 15:33:00 -0400

    Initial Commit

---
+++ MoveMe/MoveMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMeApp.swift
+//  MoveMe
+//
+//  Created by User on 2024-06-29.
+//
+
+import SwiftUI
+
+@main
+struct MoveMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
